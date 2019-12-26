a5819572bb670d9fb6c14f51c3756b5bd96036c9
Poorva Gokhale <poorvasgokhale@gmail.com>
2019-12-26 19:05:02 +0530

    #000: Poorva : upgrade esw sha

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "8634d64a"
+  private val Version = "14bc2645"
 //  private val Version = "0.1.0-SNAPSHOT"
 
   val `esw-ocs-dsl-kt` = Org             %% "esw-ocs-dsl-kt" % Version
