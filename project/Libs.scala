01843eef3462f435556d959ef081b3654834d2e2
Shubham Jaybhaye <shubhamj7997@gmail.com>
2020-03-19 14:42:19 +0530

    #ESW-296 : Shubham : Preparation of 0.1.0 release.

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "0.1.0-RC3"
+  private val Version = "0.1.0"
 
   val `esw-ocs-dsl-kt` = Org             %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org             %% "esw-ocs-app"    % Version
