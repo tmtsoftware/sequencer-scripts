a6acd2e26c858fca94758bdb9310ed5044453356
Poorva Gokhale <poorvasgokhale@gmail.com>
2020-06-23 14:26:00 +0530

    upgrade esw version

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "96ace4e"
+  private val Version = "e5f7eb301e6c7315b2f79a72752dffa8a5954cce"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
