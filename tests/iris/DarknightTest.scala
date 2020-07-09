db6d7b3fc5300f3b87487b3e92ea180e5dd9c207
weiss <weiss.personal@gmail.com>
2020-07-08 19:37:25 -0700

    JLW: tests for test command of IRIS darknight mode

--- tests/iris/DarknightTest.scala
+++ tests/iris/DarknightTest.scala
@@ -1,21 +1,37 @@
 package iris
 
-import csw.command.client.SequencerCommandServiceImpl
-import csw.params.commands.CommandResponse.Completed
+import csw.params.commands.CommandResponse.{Completed, Started}
 import csw.params.commands.{CommandName, Sequence, Setup}
 import csw.prefix.models.Prefix
 import csw.prefix.models.Subsystem.IRIS
+import esw.ocs.api.SequencerApi
 import esw.ocs.api.models.ObsMode
 import esw.ocs.testkit.EswTestKit
 
 class DarknightTest extends EswTestKit {
-  "should submit and process sequence | ESW-190, ESW-148" in {
-    val sequencerLocation = spawnSequencer(IRIS, ObsMode("Darknight"))
+  private var irisSequencerClient: SequencerApi = _
+  private val subsystem = IRIS
+  private val obsMode = ObsMode("darknight")
+  private val command1 = Setup(Prefix("esw.test"), CommandName("test"), None)
+  private val sequence = Sequence(command1)
 
-    val command1 = Setup(Prefix("esw.test"), CommandName("test"), None)
-    val sequence = Sequence(command1)
 
-    val sequencerCommandService = new SequencerCommandServiceImpl(sequencerLocation)
-    sequencerCommandService.submitAndWait(sequence).futureValue shouldBe a[Completed]
+  override protected def beforeEach(): Unit = {
+    spawnSequencer(subsystem, obsMode)
+    irisSequencerClient = sequencerClient(subsystem, obsMode)
+  }
+
+  override protected def afterEach(): Unit = shutdownAllSequencers()
+
+  "should submitAndWait sequence and get Completed" in {
+
+    irisSequencerClient.submitAndWait(sequence).futureValue shouldBe a[Completed]
+  }
+
+  "should submit sequence and get Started and then Completed on queryFinal" in {
+
+    val submitResponse = irisSequencerClient.submit(sequence).futureValue
+    submitResponse shouldBe a[Started]
+    irisSequencerClient.queryFinal(submitResponse.runId).futureValue shouldBe a[Completed]
   }
 }
