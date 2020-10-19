6f154798e60c21069d4a9dd8806615ba5f4ecb19
weiss <weiss.personal@gmail.com>
2020-10-19 10:02:52 -0700

    Port of IRIS exposure script developed for FDR

---
+++ scripts/iris/IrisConstants.kt
@@ -0,0 +1,128 @@
+package iris
+
+import csw.params.core.generics.Key
+import csw.params.core.models.Choice
+import csw.params.core.models.Choices
+import csw.params.events.EventName
+import csw.prefix.models.Prefix
+import esw.ocs.dsl.highlevel.models.IRIS
+import esw.ocs.dsl.params.*
+
+object IrisConstants {
+    val samplingModeChoices: Choices = choicesOf("CDS", "MCD", "UTR")
+
+    fun imagerFilterPositions(filter: String): List<String> = when (filter)  {
+        "H" -> listOf("H", "OPEN", "OPEN", "OPEN", "OPEN")
+        "J" -> listOf("J", "OPEN", "OPEN", "OPEN", "OPEN")
+        "K" -> listOf("K", "OPEN", "OPEN", "OPEN", "OPEN")
+        "CO" -> listOf("OPEN", "OPEN", "OPEN", "OPEN", "CO")
+        "Hn1" -> listOf("OPEN", "Hn1", "OPEN", "OPEN", "OPEN")
+        else -> listOf() // invalid (how to handle?)
+    }
+
+    val filterChoices: Choices = choicesOf("Z", "Y", "J", "H", "K", "Ks", "H+K notch", "CO",
+            "BrGamma", "PaBeta", "H2", "FeII", "HeI", "CaII Trip", "J Cont", "H Cont", "K Cont",
+            "Zn1", "Zn2", "Zn3", "Zn4", "Yn1", "Yn2", "Yn3", "Yn4", "Jn1", "Jn2", "Jn3", "Jn4", "Jn5",
+            "Hn1", "Hn2", "Hn3", "Hn4", "Hn5", "Kn1", "Kn2", "Kn3", "Kn4", "Kn5")
+
+    val scaleChoices: Choices = choicesOf("4", "9", "25", "50")
+
+    val spectralResolutionChoices: Choices = choicesOf("4000-Z", "4000-Y", "4000-J", "4000-H", "4000-K",
+            "4000-H+K", "8000-Z", "8000-Y", "8000-J", "8000-H", "8000-Kn1-3", "8000-Kn4-5", "8000-Kbb", "10000-Z",
+            "10000-Y", "10000-J", "10000-H", "10000-K", "Mirror")
+
+    object iseq {
+        val componentName = "ici.is"
+        val prefixStr = Prefix(IRIS, componentName).toString()
+
+        val imagerObserveChoices: Choices = choicesOf("START", "STOP", "ABORT")
+
+        object command {
+            val filterKey: Key<Choice> = choiceKey("filter", filterChoices)
+            val scaleKey: Key<Choice>           = choiceKey("scale", scaleChoices)
+            val resolutionKey: Key<Choice>      = choiceKey("spectralResolution", spectralResolutionChoices)
+            val imagerItimeKey: Key<Int> = intKey("imagerIntegrationTime")
+            val imagerRampsKey: Key<Int> = intKey("imagerNumRamps")
+            val imagerRepeatsKey: Key<Int>     = intKey("imagerNumRepeats")
+            val ifsItimeKey: Key<Int>          = intKey("ifsIntegrationTime")
+            val ifsRampsKey: Key<Int>          = intKey("ifsNumRamps")
+            val ifsRepeatsKey: Key<Int>        = intKey("ifsNumRepeats")
+            val ifsConfigurationsKey: Key<Int> = intKey("ifsConfigurations")
+
+            val imagerObserveKey: Key<Choice> = choiceKey("imagerObserve", imagerObserveChoices)
+        }
+        object event {
+            val observerKeywordsEvent = EventName("observerKeywords")
+        }
+    }
+
+    object sciFilterAssembly {
+        val componentName = "imager.filter"
+
+        val filterWheelNames = listOf("wheel1", "wheel2", "wheel3", "wheel4", "wheel5")
+
+        object command {
+            val wheelKeys: List<Key<String>> = filterWheelNames.map{stringKey(it)} // really an enum key
+        }
+        object event { }
+    }
+
+    object sciScaleAssembly {
+        val componentName = "ifs.scale"
+
+        object command {
+            val scaleKey: Key<Choice> = choiceKey("scale", scaleChoices)
+        }
+        object event { }
+    }
+
+    object sciResolutionAssembly {
+        val componentName = "ifs.res"
+
+        object command {
+            val spectralResolutionKey: Key<Choice> = choiceKey("spectralresolution", spectralResolutionChoices)
+        }
+        object event { }
+    }
+
+    object imagerDetectorAssembly {
+        val componentName = "imager.detector"
+
+        object command {
+            val itimeKey: Key<Int> = intKey("rampIntegrationTime")
+            val rampsKey: Key<Int> = intKey("ramps")
+            val samplingModeKey: Key<Choice> = choiceKey("imagerSamplingMode", samplingModeChoices)
+        }
+        object event {
+            val exposureInProgressKey: Key<Boolean> = booleanKey("exposureInProgress")
+
+        }
+    }
+
+    object ifsDetectorAssembly {
+        val componentName = "ifs.detector"
+
+        object command {
+            val itimeKey: Key<Int> = intKey("rampIntegrationTime")
+            val rampsKey: Key<Int> = intKey("ramps")
+            val samplingModeKey: Key<Choice> = choiceKey("imagerSamplingMode", samplingModeChoices)
+        }
+        object event {
+            val exposureInProgressKey: Key<Boolean> = booleanKey("exposureInProgress")
+
+        }
+    }
+
+    object cryoenvAssembly {
+        val componentName = "sc.cryoenv"
+
+        val cryoenvStateEventNames: Set<String> = setOf("IMG_STATE", "IFS_STATE", "WIN_STATE", "PV_STATE", "PRESS_STATE")
+        val cryoenvVacuumStates: Choices = choicesOf("WARM", "PUMPING", "WARM_VACUUM", "COOLING", "COLD", "WARMING", "PRESSURIZING")
+
+        object command { }
+        object event {
+            val cryoenvVacuumStateKey: Key<Choice> = choiceKey("thermalVacuumState", cryoenvVacuumStates)
+        }
+    }
+
+}
---
+++ scripts/iris/IrisImagerAndIfs.kts
@@ -0,0 +1,148 @@
+package iris
+
+import csw.prefix.models.Prefix
+import csw.prefix.models.Subsystem
+import esw.ocs.dsl.core.script
+import esw.ocs.dsl.highlevel.models.IRIS
+import esw.ocs.dsl.par
+import esw.ocs.dsl.params.*
+import iris.IrisConstants.ifsDetectorAssembly.command.itimeKey as ifsItimeKey
+import iris.IrisConstants.ifsDetectorAssembly.command.rampsKey as ifsRampsKey
+import iris.IrisConstants.imagerDetectorAssembly.command.itimeKey as imagerItimeKey
+import iris.IrisConstants.imagerDetectorAssembly.command.rampsKey as imagerRampsKey
+import iris.IrisConstants.iseq.command.ifsConfigurationsKey as seqIfsConfigurationsKey
+import iris.IrisConstants.iseq.command.ifsItimeKey as seqIfsItimeKey
+import iris.IrisConstants.iseq.command.ifsRampsKey as seqIfsRampsKey
+import iris.IrisConstants.iseq.command.ifsRepeatsKey as seqIfsRepeatsKey
+import iris.IrisConstants.iseq.command.imagerRampsKey as seqImagerRampsKey
+import iris.IrisConstants.iseq.command.imagerRepeatsKey as seqImagerRepeatsKey
+import iris.IrisConstants.iseq.command.imagerItimeKey as seqImagerItimeKey
+import iris.IrisConstants.iseq.command.scaleKey as seqScaleKey
+import iris.IrisConstants.iseq.command.filterKey as seqFilterKey
+import iris.IrisConstants.iseq.command.resolutionKey as seqResolutionKey
+import iris.IrisConstants.sciResolutionAssembly.command.spectralResolutionKey
+import iris.IrisConstants.sciScaleAssembly.command.scaleKey
+import kotlinx.coroutines.async
+import kotlin.time.seconds
+
+
+script {
+    val filterAssembly = Assembly(IRIS, IrisConstants.sciFilterAssembly.componentName, 5.seconds)
+    val scaleAssembly = Assembly(IRIS, IrisConstants.sciScaleAssembly.componentName, 5.seconds)
+    val spectralResAssembly = Assembly(IRIS, IrisConstants.sciResolutionAssembly.componentName, 5.seconds)
+    val imagerDetectorAssembly = Assembly(IRIS, IrisConstants.imagerDetectorAssembly.componentName, 5.seconds)
+    val ifsDetectorAssembly = Assembly(IRIS, IrisConstants.ifsDetectorAssembly.componentName, 5.seconds)
+
+    fun prefixStr(subsystem: Subsystem, componentName: String) = Prefix(subsystem, componentName).toString()
+
+
+
+    onSetup("parallelObservation") { command ->
+// configure IRIS filter (and other upstream optics)
+        val filter = command(seqFilterKey)
+        val filterResponse = async {
+            filterAssembly.submit(Setup(IrisConstants.iseq.prefixStr,
+                    "setFilter", command.obsId).add(filter))
+        }
+        // configure IFS
+        val scales = command(seqScaleKey)
+        val resolutions = command(seqResolutionKey)
+        val scaleAndResolutionResponses = async {
+            par(
+                    {
+                        scaleAssembly.submit(Setup(IrisConstants.iseq.prefixStr, "SELECT", command.obsId)
+                                .add(scaleKey.set(scales.head())))
+                    },
+                    {
+                        spectralResAssembly.submit(Setup(IrisConstants.iseq.prefixStr, "GRATING_SELECT", command.obsId)
+                                .add(spectralResolutionKey.set(resolutions.head())))
+                    }
+            )
+        }
+
+        // exposure parameters
+        val imagerItime = command(seqImagerItimeKey)
+        val imagerRamps = command(seqImagerRampsKey)
+        val imagerRepeats = command(seqImagerRepeatsKey).head()
+
+        val numIfsConfigs = command(seqIfsConfigurationsKey).head()
+        val ifsItimes = command(seqIfsItimeKey)
+        val ifsRamps = command(seqIfsRampsKey)
+        val ifsRepeats = command(seqIfsRepeatsKey)
+
+
+        // filter needs to finish, but IFS doesn't
+        val response = filterResponse.await()
+
+        // start Imager Exposure loop
+        // Imager loop
+        var imagerExposureCounter = 0
+        val imagerExposureLoop = loop {
+            // configure exposure
+            imagerDetectorAssembly.submit(
+                    Setup(IrisConstants.iseq.prefixStr, "configureExposure", command.obsId)
+                            .add(imagerItimeKey.set(imagerItime.get(imagerExposureCounter).get()))
+                            .add(imagerRampsKey.set(imagerRamps.get(imagerExposureCounter).get()))
+            )
+
+            // observe (assumes completes when exposure is complete)
+            imagerDetectorAssembly
+                    .submit(Observe(IrisConstants.iseq.prefixStr, "observe", command.obsId))
+
+            imagerExposureCounter += 1
+
+            stopWhen(imagerExposureCounter == imagerRepeats)
+        }
+
+
+        scaleAndResolutionResponses.await()
+
+        // start IFS Exposure Loop
+        var ifsConfigurationCounter = 0
+        val ifsExposureLoop = loop {
+            if (ifsConfigurationCounter > 1) {
+                // configure IFS settings
+                par(
+                        {
+                            scaleAssembly.submit(
+                                    Setup(IrisConstants.iseq.prefixStr, "SELECT", command.obsId)
+                                            .add(scaleKey.set(scales.get(ifsConfigurationCounter).get()))
+                            )
+                        },
+                        {
+                            spectralResAssembly.submit(
+                                    Setup(IrisConstants.iseq.prefixStr, "GRATING_SELECT", command.obsId)
+                                            .add(spectralResolutionKey.set(resolutions.get(ifsConfigurationCounter).get()))
+                            )
+
+                        }
+                )
+            }
+
+            var ifsExposureCounter = 0
+            loop {
+                // configure exposure
+                ifsDetectorAssembly
+                        .submit(
+                                Setup(IrisConstants.iseq.prefixStr, "configureExposure", command.obsId)
+                                        .add(ifsItimeKey.set(ifsItimes.get(ifsExposureCounter).get()))
+                                        .add(ifsRampsKey.set(ifsRamps.get(ifsExposureCounter).get()))
+                        )
+                // observe (assumes completes when exposure is complete)
+                ifsDetectorAssembly.submit(Observe(IrisConstants.iseq.prefixStr, "observe", command.obsId))
+
+                ifsExposureCounter += 1
+
+                stopWhen(ifsExposureCounter == ifsRepeats.get(ifsExposureCounter).get())
+            }
+
+            ifsConfigurationCounter += 1
+
+            stopWhen(ifsConfigurationCounter == numIfsConfigs)
+
+        }
+
+        // if results are successful, updated CRM
+        //addOrUpdateCommand(CommandResponse.Completed(command.runId))
+    }
+}
