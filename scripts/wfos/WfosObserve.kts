14e93aa95159ce422322569f6bab67952d56881d
weiss <weiss.personal@gmail.com>
2021-02-22 16:48:02 -0800

    WFOS observe scripts

---
+++ scripts/wfos/WfosObserve.kts
@@ -0,0 +1,26 @@
+package wfos
+
+import esw.ocs.dsl.core.script
+import esw.ocs.dsl.highlevel.models.WFOS
+import esw.ocs.dsl.params.intKey
+import esw.ocs.dsl.params.invoke
+
+script {
+    var stopObserving = false
+    val detectorAssembly = Assembly(WFOS, "detectorAssembly")
+
+    onObserve("observe") { command ->
+        val repeats = command(intKey("repeats")).head()
+        var counter = 0
+        loopAsync {
+            detectorAssembly.submitAndWait(Setup(prefix, "takeExposure", command.obsId))
+            counter += 1
+            stopWhen((counter == repeats) || stopObserving)
+        }
+    }
+
+    onAbortSequence {
+        stopObserving = true
+        detectorAssembly.submitAndWait(Setup(prefix, "abortExposure"))
+    }
+}
