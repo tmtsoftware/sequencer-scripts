a332c0280de027478fea03e79d1f9144d0be2e1e
Poorva Gokhale <poorvasgokhale@gmail.com>
2021-03-01 17:16:47 +0530

    #000: Poorva: Update esw version to latest sha

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "0.2.1"
+  private val Version = "ff4de77b78"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
