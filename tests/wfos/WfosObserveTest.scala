19b96ff42e41ef91d2a4ee0a8a787c98d7a48b7e
weiss <weiss.personal@gmail.com>
2021-03-09 13:04:16 -0800

    Updated assembly and tests

--- tests/wfos/WfosObserveTest.scala
+++ tests/wfos/WfosObserveTest.scala
@@ -1,56 +1,61 @@
 package wfos
 
 import akka.actor.typed.scaladsl.ActorContext
-import com.typesafe.config.ConfigFactory
 import csw.command.client.messages.TopLevelActorMessage
 import csw.framework.models.CswContext
-import csw.framework.scaladsl.{ComponentBehaviorFactory, ComponentHandlers}
+import csw.framework.scaladsl.ComponentHandlers
 import csw.location.api.models.TrackingEvent
 import csw.params.commands.CommandResponse.Completed
-import csw.params.commands.{CommandName, CommandResponse, ControlCommand, Observe, Sequence, Setup}
+import csw.params.commands._
 import csw.params.core.generics.KeyType
 import csw.params.core.models.Id
 import csw.prefix.models.Prefix
 import csw.prefix.models.Subsystem.WFOS
 import csw.time.core.models.UTCTime
 import esw.ocs.api.SequencerApi
-import esw.ocs.api.actor.client.SequencerApiFactory
 import esw.ocs.api.models.ObsMode
 import esw.ocs.testkit.EswTestKit
 
 class WfosObserveTest extends EswTestKit {
   private var wfosSequencer: SequencerApi = _
 
+  // TODO this can be simplified using DefaultComponentHandlers when added to CSW
+  // https://github.com/tmtsoftware/csw/pull/367
+  spawnAssembly(Prefix(WFOS, "detectorAssembly"), (ctx: ActorContext[TopLevelActorMessage], cswCtx: CswContext) => new ComponentHandlers(ctx, cswCtx) {
+    override def initialize(): Unit = {}
+    override def onLocationTrackingEvent(trackingEvent: TrackingEvent): Unit = {}
+    override def validateCommand(runId: Id, controlCommand: ControlCommand): CommandResponse.ValidateCommandResponse = CommandResponse.Accepted(runId)
+    override def onSubmit(runId: Id, controlCommand: ControlCommand): CommandResponse.SubmitResponse = CommandResponse.Completed(runId)
+    override def onOneway(runId: Id, controlCommand: ControlCommand): Unit = CommandResponse.Completed(runId)
+    override def onDiagnosticMode(startTime: UTCTime, hint: String): Unit = {}
+    override def onOperationsMode(): Unit = {}
+    override def onShutdown(): Unit = {}
+    override def onGoOffline(): Unit = {}
+    override def onGoOnline(): Unit = {}
+  })
+
   override protected def beforeEach(): Unit = {
     spawnSequencer(WFOS, ObsMode("WFOS_simple"))
     wfosSequencer = sequencerClient(WFOS, ObsMode("WFOS_simple"))
-    spawnAssembly(Prefix(WFOS, "detectorAssembly"), new ComponentBehaviorFactory {
-      override protected def handlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswContext): ComponentHandlers = new ComponentHandlers(ctx, cswCtx) {
-        override def initialize(): Unit = ???
-
-        override def onLocationTrackingEvent(trackingEvent: TrackingEvent): Unit = ???
-
-        override def validateCommand(runId: Id, controlCommand: ControlCommand): CommandResponse.ValidateCommandResponse = CommandResponse.Accepted(runId)
-
-        override def onSubmit(runId: Id, controlCommand: ControlCommand): CommandResponse.SubmitResponse = CommandResponse.Completed(runId)
-
-        override def onOneway(runId: Id, controlCommand: ControlCommand): Unit = CommandResponse.Completed(runId)
-
-        override def onDiagnosticMode(startTime: UTCTime, hint: String): Unit = ???
-
-        override def onOperationsMode(): Unit = ???
-
-        override def onShutdown(): Unit = ???
-
-        override def onGoOffline(): Unit = ???
-
-        override def onGoOnline(): Unit = ???
-      }
-    })
   }
 
-  "should take exposure" in {
-    val command = Observe(Prefix("WFOS.test"), CommandName("observe")).add(KeyType.IntKey.make("repeats").set(1))
-    wfosSequencer.submitAndWait(Sequence(command)).futureValue shouldBe a[Completed]
+  override protected def afterEach(): Unit = {
+    shutdownAllSequencers()
+  }
+
+  "Repeated Observe command" should {
+    "take repeated exposures" in {
+      val command = Observe(Prefix("WFOS.test"), CommandName("repeatedObserve")).add(KeyType.IntKey.make("repeats").set(1))
+      wfosSequencer.submitAndWait(Sequence(command)).futureValue shouldBe a[Completed]
+    }
+
+    "return error when taking repeated exposure but repeats not specified" in {
+      val command = Observe(Prefix("WFOS.test"), CommandName("repeatedObserve"))
+      val response = wfosSequencer.submitAndWait(Sequence(command)).futureValue
+      response shouldBe a[CommandResponse.Error]
+      // TODO update when CSW is updated with better error message
+      // https://github.com/tmtsoftware/csw/pull/371
+      println(response.asInstanceOf[CommandResponse.Error].message)
+    }
   }
 }
