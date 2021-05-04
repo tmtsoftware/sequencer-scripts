1833054ea197a834d543fad6784932aed37d489d
Vinay Mahamuni <vinaymahamuni22@gmail.com>
2021-05-04 15:19:14 +0530

    #ESW-000: Vinay| Manish: use 0.1.0-SNAPSHOT version for esw if argument -Ddev=true is passed to the sbt

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,8 +2,12 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "14acf9149"
-
+  private val Version = {
+    sys.props.get("dev") match {
+      case Some("true") => "0.1.0-SNAPSHOT"
+      case _            => "d8c798151"
+    }
+  }
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
 }
