7c81f5446c909dfb8e067343ee2d9f2a8b4ee7ff
Shubham Chauhan <shubham25may1998@gmail.com>
2021-11-01 09:07:06 +0530

    #ESW-551: Chauhan: Refactor and extract common code

---
+++ scripts/iris/CommonUtils.kt
@@ -0,0 +1,76 @@
+package iris
+
+import csw.params.core.generics.Key
+import csw.params.core.models.Choice
+import csw.params.core.models.ExposureId
+import csw.params.core.models.ObsId
+import csw.params.events.EventKey
+import csw.params.events.EventName
+import csw.params.events.SystemEvent
+import csw.prefix.models.Prefix
+import esw.ocs.dsl.core.CommandHandlerScope
+import esw.ocs.dsl.highlevel.RichComponent
+import esw.ocs.dsl.highlevel.models.IRIS
+import esw.ocs.dsl.isStarted
+import esw.ocs.dsl.params.Params
+import esw.ocs.dsl.params.booleanKey
+import esw.ocs.dsl.params.first
+import esw.ocs.dsl.params.invoke
+import kotlin.time.Duration
+
+suspend fun <T> CommandHandlerScope.setupAssembly(assembly: RichComponent, commandName: String, key: Key<T>, assemblyKey: Key<T>, params: Params) {
+    val assemblyParam = params.get(key)
+    if (assemblyParam.isDefined) {
+        assemblyKey.set(assemblyParam.get().first)
+        val command = Setup(assembly.prefix.toString(), commandName).add(assemblyParam.get())
+        val initialRes = assembly.submit(command)
+
+        loop(Duration.milliseconds(100)) {
+            val query = assembly.query(initialRes.runId())
+            stopWhen(!query.isStarted)
+        }
+    }
+}
+
+suspend fun CommandHandlerScope.loadConfiguration(assembly: RichComponent, obsId: ObsId?, directory: String, exposureId: ExposureId, rampIntegrationTime: Int, ramps: Int) {
+    val fileName = "${directory}/${exposureId}.fits"
+    val fileNameParam = fileNameKey.set(fileName)
+    val exposureIdParam = exposureIdKey.set(exposureId.toString())
+    val rampIntegrationTimeParam = rampIntegrationTimeKey.set(rampIntegrationTime)
+    val rampsParam = rampsKey.set(ramps)
+    val command = Setup(assembly.prefix.toString(), "LOAD_CONFIGURATION", obsId?.toString()).madd(exposureIdParam, fileNameParam, rampIntegrationTimeParam, rampsParam)
+    assembly.submitAndWait(command)
+}
+
+suspend fun CommandHandlerScope.startExposure(assembly: RichComponent, obsId: ObsId?) {
+    val initialRes = assembly.submit(Observe(assembly.prefix.toString(), "START_EXPOSURE", obsId?.toString()))
+    loop(Duration.milliseconds(100)) {
+        val query = assembly.query(initialRes.runId())
+        stopWhen(!query.isStarted)
+    }
+}
+
+suspend fun CommandHandlerScope.setupAdcAssembly(adcAssembly: RichComponent, params: Params) {
+    val followParam = params.get(scienceAdcFollowKey)
+    if (followParam.isDefined) {
+        val angle = followParam.get().first
+        val retractParam = retractSelectKey.set(Choice("IN"))
+        val retractCommand = Setup(adcAssembly.prefix.toString(), "RETRACT_SELECT").add(retractParam)
+        adcAssembly.submitAndWait(retractCommand)
+
+        val prismFollowParam = targetAngleKey.set(angle)
+        val followCommand = Setup(adcAssembly.prefix.toString(), "PRISM_FOLLOW").add(prismFollowParam)
+        adcAssembly.submitAndWait(followCommand)
+        waitFor {
+            var onTarget = false
+            onEvent(EventKey(Prefix(IRIS, "imager.adc"), EventName("prism_state")).key()) { event ->
+                when (event) {
+                    is SystemEvent -> {
+                        event(booleanKey("onTarget")).head()?.let { x -> onTarget = x }
+                    }
+                }
+            }
+            onTarget
+        }
+    }
+}
--- scripts/iris/IRIS_ImagerAndIFS.kts
+++ scripts/iris/IRIS_ImagerAndIFS.kts
@@ -1,13 +1,12 @@
 package iris
 
-import csw.params.events.SystemEvent
-import csw.params.javadsl.JUnits
 import esw.ocs.dsl.core.script
 import esw.ocs.dsl.highlevel.models.ExposureId
 import esw.ocs.dsl.highlevel.models.IRIS
 import esw.ocs.dsl.highlevel.models.ObsId
 import esw.ocs.dsl.par
-import esw.ocs.dsl.params.*
+import esw.ocs.dsl.params.invoke
+import esw.ocs.dsl.params.params
 
 script {
     val imagerAssembly = Assembly(IRIS, "imager.filter")
@@ -18,50 +17,37 @@ script {
     val ifsDetector = Assembly(IRIS, "ifs.detector")
 
 
-
     onSetup("setupObservation") { command ->
         val params = command.params
 
-        choiceKey("scale", JUnits.marcsec)
-
         par(
-                { setupAssembly(imagerAssembly, "SELECT", choiceKey("wheel1"), params) },
-                { setupAssembly(scaleAssembly, "SELECT", choiceKey("scale", JUnits.marcsec), params) },
-                { setupAssembly(gratingAssembly, "GRATING_SELECT", choiceKey("spectralResolution"), params) },
+                { setupAssembly(imagerAssembly, "SELECT", filterKey, wheel1Key, params) },
+                { setupAssembly(scaleAssembly, "SELECT", scaleKey, scaleKey, params) },
+                { setupAssembly(gratingAssembly, "GRATING_SELECT", spectralResolutionKey, spectralResolutionKey, params) },
                 { setupAdcAssembly(adcAssembly, params) }
         )
-
-        waitFor {
-            var onTarget = false
-            onEvent(EventKey("IRIS.imager.adc", "prism_state").key()) { event ->
-                when (event) {
-                    is SystemEvent -> {
-                        event(booleanKey("onTarget")).head()?.let { x -> onTarget = x }
-                    }
-                }
-            }
-            onTarget
-        }
     }
 
     onObserve("singleExposure") { command ->
-        val directory = command(stringKey("directory")).head()
+        val directory = command(directoryKey).head()
         val obsId = command.obsId?.let { id -> ObsId(id) }
 
-        val imagerExposureId = command(stringKey("imagerExposureId")).head()
-        val imagerIntegrationTime = command(intKey("imagerIntegrationTime")).head()
-        val imagerNumRamps = command(intKey("imagerNumRamps")).head()
+        val imagerExposureId = command(imagerExposureIdKey).head()
+        val imagerIntegrationTime = command(imagerIntegrationTimeKey).head()
+        val imagerNumRamps = command(imagerNumRampsKey).head()
 
-        val ifsExposureId = command(stringKey("ifsExposureId")).head()
-        val ifsIntegrationTime = command(intKey("ifsIntegrationTime")).head()
-        val ifsNumRamps = command(intKey("ifsNumRamps")).head()
+        val ifsExposureId = command(ifsExposureIdKey).head()
+        val ifsIntegrationTime = command(ifsIntegrationTimeKey).head()
+        val ifsNumRamps = command(ifsNumRampsKey).head()
 
         par(
                 { loadConfiguration(imagerDetector, obsId, directory, ExposureId(imagerExposureId), imagerIntegrationTime, imagerNumRamps) },
                 { loadConfiguration(ifsDetector, obsId, directory, ExposureId(ifsExposureId), ifsIntegrationTime, ifsNumRamps) }
         )
 
-        imagerDetector.submit(Observe(imagerDetector.prefix.toString(), "START_EXPOSURE", command.obsId))
-        ifsDetector.submit(Observe(ifsDetector.prefix.toString(), "START_EXPOSURE", command.obsId))
+        par(
+                { startExposure(imagerDetector, obsId) },
+                { startExposure(ifsDetector, obsId) }
+        )
     }
 }
