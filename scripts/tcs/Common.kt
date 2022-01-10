1d2e938995f9a12838c16bf8b6db74bee05e8129
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2022-01-10 16:26:39 +0530

    #ESW-569 : Manish | Himanshu : Use Angle distance method to calculate getMountPositionError as per QA feedback.

--- scripts/tcs/Common.kt
+++ scripts/tcs/Common.kt
@@ -1,13 +1,11 @@
 import common.currentAltAzCoordKey
 import common.demandAltAzCoordKey
+import csw.params.core.models.Angle
 import csw.params.events.SystemEvent
 import esw.ocs.dsl.params.invoke
 
 fun getMountPositionError(event: SystemEvent): Double {
     val current = event(currentAltAzCoordKey).head()
     val demand = event(demandAltAzCoordKey).head()
-
-    val atlDiff = current.alt().`$minus`(demand.alt())
-    val azDiff = current.az().`$minus`(demand.az())
-    return atlDiff.`$times`(2).`$plus`(azDiff.`$times`(2)).toDegree()
+    return Angle.distance(current.alt().toRadian(), current.az().toRadian(), demand.alt().toRadian(), demand.az().toRadian())
 }
