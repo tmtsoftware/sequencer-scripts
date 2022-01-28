08f861715da2d17f47e782053b57fec7391b4cfb
rajnov08 <rajnov08@gmail.com>
2022-01-28 16:04:58 +0530

    Rajalakshmi|ESW-568 - Script changes for variation in IRIS red/blue to imager/ifs

--- scripts/esw/IRIS_ImagerAndIFS.kts
+++ scripts/esw/IRIS_ImagerAndIFS.kts
@@ -9,16 +9,16 @@ import esw.ocs.dsl.highlevel.models.IRIS
 script {
     println("[ESW IRIS_ImagerAndIFS]: script loaded")
 
-    val irisRedSequencer = Sequencer(IRIS, obsMode, Variation("red"))
-    val irisBlueSequencer = Sequencer(IRIS, obsMode, Variation("blue"))
+    val irisImagerSequencer = Sequencer(IRIS, obsMode, Variation("IRIS_IMAGER"))
+    val irisIfsSequencer = Sequencer(IRIS, obsMode, Variation("IRIS_IFS"))
     val delayDuration = 30000L //30sec
 
     onSetup("setup-iris-variation") { command1 ->
         println("[ESW IRIS_ImagerAndIFS]: setup-iris-variation command received")
         val command2 = getIrisCommand2(command1.source(), command1.obsId)
         val command3 = getIrisCommand3(command1.source(), command1.obsId)
-        irisRedSequencer.submitAndWait(sequenceOf(command2))
-        irisBlueSequencer.submitAndWait(sequenceOf(command3))
+        irisImagerSequencer.submitAndWait(sequenceOf(command2))
+        irisIfsSequencer.submitAndWait(sequenceOf(command3))
         println("[ESW IRIS_ImagerAndIFS]: setup-iris-variation command completed")
     }
 
---
+++ scripts/iris/IRIS_ImagerAndIFS_Ifs.kts
@@ -0,0 +1,16 @@
+package iris
+
+import esw.ocs.dsl.core.script
+import kotlinx.coroutines.delay
+
+script {
+    println("[IRIS IRIS_ImagerAndIFS_Ifs]: script loaded")
+
+    val delayDuration = 10000L //10sec
+
+    onObserve("iris-command3") {
+        println("[IRIS IRIS_ImagerAndIFS_Ifs]: iris-command3 command received")
+        delay(delayDuration)
+        println("[IRIS IRIS_ImagerAndIFS_Ifs]: iris-command3 command completed")
+    }
+}
