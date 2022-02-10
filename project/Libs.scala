f7ced8060d4fa2e6dede0e22bd5dc1db303dc991
Shubham Chauhan <shubham25may1998@gmail.com>
2022-02-10 20:37:14 +0530

    #CSW-168: Chauhan: Upgrade to support jvm 17

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "0.4.0"
+  private val Version = "f3d073f"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
