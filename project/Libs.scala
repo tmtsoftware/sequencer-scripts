625e16b31546da1b380c85a4d2ba35bdb8e92f71
Shubham Chauhan <shubham25may1998@gmail.com>
2022-02-14 11:16:50 +0530

    Revert "#CSW-168: Chauhan: Upgrade to support jvm 17"
    This reverts commit bfb76bfbcdc4390a52c9179b73889f63b94b0a32.

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "f3d073f"
+  private val Version = "0.4.0-RC1"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
