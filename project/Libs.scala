e04773235526d39f817c04702c4f8ac85ee16045
Shubham Chauhan <shubham25may1998@gmail.com>
2022-02-15 23:11:58 +0530

    #000: Chauhan: Update esw-sha and kotlin sha

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "f3d073f"
+  private val Version = "202f084a74"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
