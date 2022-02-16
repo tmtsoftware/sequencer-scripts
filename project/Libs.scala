917e204778a28d960289314a6c0c7da7c461f492
manish <gowardipemanish95@gmail.com>
2022-02-16 16:49:55 +0530

    #000: Chauhan: Update esw-sha

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "3ec7078"
+  private val Version = "3b1cd4240f"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
