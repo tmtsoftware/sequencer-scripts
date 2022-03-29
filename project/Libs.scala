6e981ce7954c96710ff5d12c87cbb4552ef334ec
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2022-03-29 15:14:09 +0530

    #000: Himanshu : Update esw-sha

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "f93d3c5"
+  private val Version = "8a1aec7"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
