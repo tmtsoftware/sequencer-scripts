c051ed51284bef24ba92b4b0edfe5e5bc2537128
Allan Brighton <Allan.Brighton@t-online.de>
2022-06-13 18:22:08 +0200

    Start of graalpython test

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,8 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "1ea12a9"
+//  private val Version = "1ea12a9"
+  private val Version = "0.1.0-SNAPSHOT"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
