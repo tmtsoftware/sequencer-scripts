7a0c152529123113d2c57a59ea93e49fca7bed8b
Allan Brighton <Allan.Brighton@t-online.de>
2023-11-08 09:53:04 +0100

    Changed esw version

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Libs {
   private val Org     = "com.github.tmtsoftware.esw"
-  private val Version = "4b71223"
+  private val Version = "e84e4d4"
 
   val `esw-ocs-dsl-kt` = Org %% "esw-ocs-dsl-kt" % Version
   val `esw-ocs-app`    = Org %% "esw-ocs-app"    % Version
